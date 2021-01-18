41fa704eaec213342f7bd02bdb0e4ba8d2fa9c94
Jeyluu <73641462+Jeyluu@users.noreply.github.com>
2021-01-18 12:30:32 +0100

    Hashed PW Done next v8

--- projet final jluu/models/users.js
+++ projet final jluu/models/users.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail }  = require("validator") //pk isEmail est entre crochet?
-
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -17,6 +17,18 @@ const userSchema = new mongoose.Schema({
     },
 });
 
+//Dire a mongoose de faire une fonction après qu'un nouvel utilisateur ai été créé
+/*userSchema.post('save', function (doc,next) { //La methode post utilisé sur cette ligne est la méthode "après" et non poster
+    console.log('Un nouvel utilisateur a été créé et sauvegardé', doc);
+    next();
+})*/
+
+//Dire a mongoose de faire une fonction avant qu'un nouvel utilisateur ai été créé
+userSchema.pre('save', async function (next) { //La methode post utilisé sur cette ligne est la méthode "avant" et non poster
+    const salt = await bcrypt.genSalt()  // asynchrone donc on met await mais pourquoi?
+    this.password = await bcrypt.hash(this.password, salt)                                //this = refere à la creation de compte
+    next();
+})
 const User = mongoose.model("customer", userSchema)
 
 module.exports = User;
